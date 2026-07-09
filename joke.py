eebbb81933da83bc02909abb1163563366962f07
Hemalatha Palanisamy <hema.latha765@gmail.com>
2026-07-09 15:56:16 +0000

    Evaluation file

---
+++ joke.py
@@ -0,0 +1,33 @@
+"""Run this model in Python
+
+> pip install openai
+"""
+import os
+from openai import OpenAI
+
+# To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
+# Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+client = OpenAI(
+    base_url="https://models.github.ai/inference",
+    api_key=os.environ["GITHUB_TOKEN"],
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "",
+        },
+        {
+            "role": "user",
+            "content": """Explain this joke: I was going to fly to visit my family on May 3rd. My mom said "Oh great, your dad's poetry reading is that night!" So now I'm flying in on May 4th.""",
+        }
+    ],
+    model="openai/gpt-4o-mini",
+    temperature=1,
+    max_tokens=4096,
+    top_p=1
+)
+
+print(response.choices[0].message.content)
+
